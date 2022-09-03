b54f5a3eaec3b53ec1e807cdce512e36f88ef9a9
Merst <noxlittner@gmail.com>
2022-09-03 15:17:49 -0400

    removal of seckey

--- plunty/settings.py
+++ plunty/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u^bm6ce^g82f^dk=w#_#nr4+^fd_+&24a@^w*1-0suqlfhtbcp'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
